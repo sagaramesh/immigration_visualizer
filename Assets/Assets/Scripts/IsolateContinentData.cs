407d3ab4f522d47ab457b03c98872dd59b3308d2
Sagar Ramesh <sagarram@usc.edu>
2017-04-22 12:48:21 -0700

    Updated IsolateContinentData to regenerate all parents

--- Assets/Assets/Scripts/IsolateContinentData.cs
+++ Assets/Assets/Scripts/IsolateContinentData.cs
@@ -34,6 +34,9 @@ public class IsolateContinentData : MonoBehaviour {
 		else if(Input.GetKeyDown (KeyCode.Alpha6)){
 			IsolateOceania ();
 		}
+		else if(Input.GetKeyDown (KeyCode.Alpha7)){
+			IsolateNothing ();
+		}
 		else {
 			print ("Nothing isolated");
 		}
@@ -95,6 +98,16 @@ public class IsolateContinentData : MonoBehaviour {
 		c_Africa.africaParent.SetActive (false);
 		d_Europe.europeParent.SetActive (false);
 		e_Asia.asiaParent.SetActive (false);
+		f_Oceania.oceaniaParent.SetActive (true);
+
+	}
+	public void IsolateNothing(){
+
+		a_NorthAmerica.northAmericaParent.SetActive (true);
+		b_LatinAmerica.latinAmericaParent.SetActive (true);
+		c_Africa.africaParent.SetActive (true);
+		d_Europe.europeParent.SetActive (true);
+		e_Asia.asiaParent.SetActive (true);
 		f_Oceania.oceaniaParent.SetActive (true);
 
 	}
