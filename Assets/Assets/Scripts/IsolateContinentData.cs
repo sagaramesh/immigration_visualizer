71fc03c7e5845a1a6e32249ffefec58741cafca6
MARTINELLIS\demoadmin <dokenfuss@gmail.com>
2017-04-23 18:13:39 -0700

    Continent isolation buttons are now colored when triggered. Need to do the same for the year buttons.

--- Assets/Assets/Scripts/IsolateContinentData.cs
+++ Assets/Assets/Scripts/IsolateContinentData.cs
@@ -8,6 +8,22 @@ public class IsolateContinentData : MonoBehaviour {
     public NVRButton Button;
     public int continentNumber;
 
+    public GameObject africaLabel;
+    public GameObject europeLabel;
+    public GameObject asiaLabel;
+    public GameObject latinAmericaLabel;
+    public GameObject oceaniaLabel;
+    public GameObject northAmericaLabel;
+
+    public GameObject africaBottom;
+    public GameObject europeBottom;
+    public GameObject asiaBottom;
+    public GameObject latinAmericaBottom;
+    public GameObject oceaniaBottom;
+    public GameObject northAmericaBottom;
+
+
+
     // Use this for initialization
     void Start () {
 		GameObject scriptContainer = GameObject.Find("ScriptContainer"); 
@@ -22,31 +38,129 @@ public class IsolateContinentData : MonoBehaviour {
 
 		if (Button.ButtonDown && continentNumber == 1) {
 			IsolateAfrica ();
-		} 
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 218, 139, 255); // Modify
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 218, 139, 255); // Modify
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        } 
 		else if(Button.ButtonDown && continentNumber == 2)
         {
-			IsolateEurope ();
-		}
+			IsolateEurope (); // 169 235
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 169, 235, 255); // Modify
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 169, 235, 255); // Modify
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        }
 		else if(Button.ButtonDown && continentNumber == 3)
         {
 			IsolateAsia ();
-		}
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255); 
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(124, 255, 82, 255); // Modify
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255); 
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(124, 255, 82, 255); // Modify
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        }
 		else if(Button.ButtonDown && continentNumber == 4)
         {
 			IsolateLatinAmerica ();
-		}
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(77, 211, 255, 255); // Modify
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(77, 211, 255, 255); // Modify
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        }
 		else if(Button.ButtonDown && continentNumber == 5)
         {
 			IsolateOceania ();
-		}
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(81, 0, 199, 255); // Modify
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(81, 0, 199, 255); // Modify
+        }
 		else if(Button.ButtonDown && continentNumber == 6)
         {
 			IsolateNorthAmerica ();
-		}
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(241, 255, 0, 255); // Modify
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(241, 255, 0, 255); // Modify
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        }
 		else if(Button.ButtonDown && continentNumber == 7)
         {
 			IsolateNothing ();
-		}
+
+            northAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaLabel.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+
+            northAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            latinAmericaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            africaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            europeBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            asiaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+            oceaniaBottom.GetComponent<SpriteRenderer>().material.color = new Color32(255, 255, 255, 255);
+        }
 		else {
 			print ("Nothing isolated");
 		}
@@ -78,9 +192,9 @@ public class IsolateContinentData : MonoBehaviour {
 		c_Africa.africaParent.SetActive (true);
 		d_Europe.europeParent.SetActive (false);
 		e_Asia.asiaParent.SetActive (false);
-		f_Oceania.oceaniaParent.SetActive (false);
+        f_Oceania.oceaniaParent.SetActive(false);
 
-	}
+    }
 	public void IsolateEurope(){
 
 		a_NorthAmerica.northAmericaParent.SetActive (false);
@@ -118,7 +232,6 @@ public class IsolateContinentData : MonoBehaviour {
 		c_Africa.africaParent.SetActive (true);
 		d_Europe.europeParent.SetActive (true);
 		e_Asia.asiaParent.SetActive (true);
-		f_Oceania.oceaniaParent.SetActive (true);
-
-	}
+        f_Oceania.oceaniaParent.SetActive(true);
+    }
 }
