1b3573659bae4961f2d63f5ec2accf81a134511c
Sagar Ramesh <sagarram@usc.edu>
2017-04-26 23:12:59 -0700

    Began work on fading reverse sphere in and out

---
+++ Assets/Assets/Scripts/ActivateSphere.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSphere : MonoBehaviour {
+
+	float lerpTime = 1f;
+	float currentLerpTime;
+
+	public Renderer reverseSphere;
+
+	public Color32 startColor = Color.white;
+	public Color32 endColor = Color.black;
+	public Color32 lerpedColor;
+
+	void Update() {
+
+		//Color tempColor = reverseSphere.GetComponent<Renderer> ().material.color;
+		//tempColor.a = Mathf.MoveTowards(0, 1, Time.deltaTime);
+		//reverseSphere.GetComponent<Renderer> ().material.color = tempColor;
+
+		currentLerpTime += Time.deltaTime;
+		if (currentLerpTime > lerpTime) {
+			currentLerpTime = lerpTime;
+		}
+
+		float percentage = currentLerpTime / lerpTime;
+		lerpedColor = Color32.Lerp(startColor, endColor, percentage);
+	}
+}
