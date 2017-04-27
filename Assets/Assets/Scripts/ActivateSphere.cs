c09ee3dbfa2819037bb563b6d83896fc9d50416e
Sagar Ramesh <sagarram@usc.edu>
2017-04-27 12:42:21 -0700

    Info button button and message working, info label and exit label switch out according to info message state

--- Assets/Assets/Scripts/ActivateSphere.cs
+++ Assets/Assets/Scripts/ActivateSphere.cs
@@ -1,30 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NewtonVR;
 
 public class ActivateSphere : MonoBehaviour {
+	
+	public NVRButton Button;
 
-	float lerpTime = 1f;
-	float currentLerpTime;
+	public GameObject sphere;
+	public GameObject info_message;
 
-	public Renderer reverseSphere;
+	public GameObject info_label;
+	public GameObject exit_label;
 
-	public Color32 startColor = Color.white;
-	public Color32 endColor = Color.black;
-	public Color32 lerpedColor;
+	float infolabelnumber;
+	float exitlabelnumber;
+
+	// 1 is active, 2 is inactive 
+
+	//private float sphereDuration = 1f;
+
+	void Start(){
+		info_message.SetActive (false);
+
+		infolabelnumber = 1;
+		exitlabelnumber = 2;
+	}
 
 	void Update() {
 
-		//Color tempColor = reverseSphere.GetComponent<Renderer> ().material.color;
-		//tempColor.a = Mathf.MoveTowards(0, 1, Time.deltaTime);
-		//reverseSphere.GetComponent<Renderer> ().material.color = tempColor;
+		// INFO AND EXIT LABEL CONDITIONS
 
-		currentLerpTime += Time.deltaTime;
-		if (currentLerpTime > lerpTime) {
-			currentLerpTime = lerpTime;
+		if (infolabelnumber == 1) {
+			info_label.SetActive (true);
+		} else if (infolabelnumber == 2) {
+			info_label.SetActive (false);
+		} else {
+			info_label.SetActive (false);
+		}
+		if (exitlabelnumber == 1) {
+			exit_label.SetActive (true);
+		} else if (exitlabelnumber == 2) {
+			exit_label.SetActive (false);
+		} else {
+			exit_label.SetActive (false);
 		}
 
-		float percentage = currentLerpTime / lerpTime;
-		lerpedColor = Color32.Lerp(startColor, endColor, percentage);
+		// BUTTON CONDITIONS
+
+		if(Button.ButtonDown && infolabelnumber == 1 || Input.GetKeyDown(KeyCode.T)) // Info and Sphere at full opacity  
+		{
+			StartCoroutine(InfoFader(1.0f, 1.0f));
+			StartCoroutine(SphereFader(1.0f, 1.0f));
+
+			infolabelnumber = 2;
+			exitlabelnumber = 1;
+		}
+		if(Button.ButtonDown && exitlabelnumber == 1 || Input.GetKeyDown(KeyCode.F)) // Info and sphere at zero opacity
+		{
+			StartCoroutine(InfoFader(0.0f, 1.0f));
+			StartCoroutine(SphereFader(0.0f, 1.0f));
+
+			infolabelnumber = 1;
+			exitlabelnumber = 2;
+		}
 	}
+
+	IEnumerator SphereFader(float sphere_aValue, float sphere_aTime)
+	{
+		float sphere_alpha = sphere.GetComponent<Renderer>().material.color.a; 
+
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / sphere_aTime) {
+			Color newColor = new Color (1, 1, 1, Mathf.Lerp (sphere_alpha, sphere_aValue, t));
+			sphere.GetComponent<Renderer> ().material.color = newColor;
+
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
+	IEnumerator InfoFader(float info_aValue, float info_aTime)
+	{
+		info_message.SetActive (true);
+
+		float info_alpha = info_message.GetComponent<SpriteRenderer>().material.color.a; 
+
+		for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / info_aTime)
+		{
+			Color newColor = new Color(1, 1, 1, Mathf.Lerp(info_alpha,info_aValue,t));
+			info_message.GetComponent<SpriteRenderer>().material.color = newColor;
+
+			yield return new WaitForEndOfFrame ();
+		}
+	}
+
 }
